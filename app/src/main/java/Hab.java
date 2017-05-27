382e84959983c09d329dc37fd246a4b9ef6c804e
Matt Rose <yamiacat@gmail.com>
2017-05-27 10:07:34 +0100

    Hab class created

---
+++ app/src/main/java/Hab.java
@@ -0,0 +1,18 @@
+/**
+ * Created by user on 27/05/2017.
+ */
+
+public class Hab {
+
+    private String habName;
+    private int size;
+
+    public Hab(String habName, int size) {
+        this.habName = habName;
+        this.size = size;
+    }
+
+    public String getHabName() {
+        return this.habName;
+    }
+}
